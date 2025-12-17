1b8e22b45417f4b7cd2687545bace4368d159f0a
Haris Rajesh <fireplatypus375@gmail.com>
2025-12-17 01:30:06 -0500

    finalized home page design updated placeholders

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,37 +1,44 @@
 "use client"
 // app/page.tsx
 import dynamic from 'next/dynamic'
+import type { ComponentType } from 'react'
 
 // Dynamic import with SSR disabled (prevents server-side render errors)
-const DotGrid = dynamic(() => import('../components/DotGrid'), { ssr: false })
-import GradientText from '../components/GradientText'
-
+const FaultyTerminal = dynamic(() => import('../components/FaultyTerminal'), { ssr: false }) as unknown as ComponentType<any>
+const TextType = dynamic(() => import('../components/TextType'), { ssr: false }) as unknown as ComponentType<any>
+const DecryptedText = dynamic(() => import('../components/DecryptedText'), { ssr: false }) as unknown as ComponentType<any>
 
 export default function Home() {
   return (
-    <div className="container">
-      <div style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: 0 }}>
-        <DotGrid
-          dotSize={3}
-          gap={25}
-          baseColor="#111111ff"
-          activeColor="#ffffffff"
-          proximity={200}
-          shockRadius={250}
-          shockStrength={5}
-          resistance={750}
-          returnDuration={1.5}
+    <div className="background">
+      <div className="wall">
+        <FaultyTerminal
+          scale={2}
+          gridMul={[2, 1]}
+          digitSize={1.5}
+          timeScale={0.5}
+          pause={false}
+          scanlineIntensity={0.5}
+          glitchAmount={0.5}
+          flickerAmount={1}
+          noiseAmp={1}
+          chromaticAberration={0}
+          dither={0}
+          curvature={0.1}
+          tint="#a7ef9e"
+          mouseReact={true}
+          mouseStrength={0.4}
+          pageLoadAnimation={true}
+          brightness={1}
         />
       </div>
       <div className="welcome">
-        <GradientText
-          colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
-          animationSpeed={10}
-          showBorder={false}
-          className="custom-class"
-          >
-          Welcome
-        </GradientText>
+        <span className="welcome-line">
+          <DecryptedText text="Harishankar" animateOn="view" revealDirection="center" speed={150} />
+        </span>
+        <span className="welcome-line">
+          <DecryptedText text="Rajesh" animateOn="view" revealDirection="center" speed={150} />
+        </span>
       </div>
     </div>
   )
